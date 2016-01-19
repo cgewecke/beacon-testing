c70255d4cc6409a375e56793f909bafc99d27c44
Christopher gewecke <christophergewecke@gmail.com>
2016-01-19 15:37:18 -0800

    Added jasmine unit test scaffolding

---
+++ linkedin/tests/services/login.tests.js
@@ -0,0 +1,8 @@
+describe('Login', function(){
+    var scope;
+
+    // tests start here
+    it('should have enabled friends to be true', function(){
+        console.log('ran test');
+    });
+});
